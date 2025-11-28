babe4dafaf28b7598cfecee5b9a6c5dfd75a1dc1
Nattsfs <feitosasalesnathan@gmail.com>
2025-11-28 20:00:13 -0300

    pagina do motoboy (adicionar botao)

---
+++ src/app/pages/motoboy/motoboy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motoboy',
+  standalone: true,
+  templateUrl: './motoboy.component.html',
+  styleUrls: ['./motoboy.component.scss']
+})
+export class MotoboyComponent {
+  constructor() {}
+}
